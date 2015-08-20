b7157dabfe7b120ab28722e7c2003f25e8b48f0b
santos22 <santosjavier22@gmail.com>
2015-08-19 19:58:57 -0500

    Deleted some tutorial code

--- js/field.js
+++ js/field.js
@@ -1,27 +1,11 @@
 function draw() {
 	var canvas = new fabric.Canvas('canvas');
-	var group = [];
 
 	fabric.Image.fromURL("http://www.clker.com/cliparts/a/v/l/j/s/Q/soccer-field.svg", function(oImg) {
+		// worry about rotation later
 		oImg.set('selectable', false);
         canvas.add(oImg);
     });
-
-	// create rectangle object
-	var field = new fabric.Rect({
-		left: 0,
-		top: 0,
-		fill: 'green',
-		width: 400,
-		height: 500
-	});
-
-	//field.set('selectable', false); // make object unselectable
-	//canvas.add(field);
-
-	// move object
-	// field.set({ left: 20, top: 50 });
-	// canvas.renderAll();
 }
 
 // window.onload = function() {
